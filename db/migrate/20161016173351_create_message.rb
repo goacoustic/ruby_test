30cb373cc44ee6d73c2cf7575407635f86640e05
we <we@ua.ua>
2016-10-17 00:11:56 +0300

    changes migration

--- db/migrate/20161016173351_create_message.rb
+++ db/migrate/20161016173351_create_message.rb
@@ -1,7 +1,8 @@
-class CreateModel < ActiveRecord::Migration[5.0]
+class CreateMessage < ActiveRecord::Migration[5.0]
    def up
   	create_table :messages do |t|
-  		t.string :name
+  		t.string :text
+      t.string :id
   	end
   end
 
